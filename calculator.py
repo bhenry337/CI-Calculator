b1b31a2e15bd4cf5e05b3427d9cb8f18b9bbeec8
bhenry337 <72040662+bhenry337@users.noreply.github.com>
2021-06-05 12:14:44 -0700

    added mult and div methods and their tests

--- calculator.py
+++ calculator.py
@@ -7,3 +7,8 @@ def add(a, b):
 def subtract(a, b):
 	return a - b
 
+def mult(a, b):
+	return a*b
+
+def div(a, b):
+	return a/b
--- test_calculator.py
+++ test_calculator.py
@@ -10,3 +10,9 @@ class TestCalculatorApp:
 
 	def test_subtract(self):
 		assert 5 == calculator.subtract(10,5)
+
+	def test_mult(self):
+		assert 12 == calculator.mult(3,4)
+
+	def test_div(self):
+		assert 2 == calculator.div(10,5)
