f13a9ed8c6a1e62ece408ad0d474c129b0d0dcc7
bhenry337 <72040662+bhenry337@users.noreply.github.com>
2021-06-05 12:24:56 -0700

    More linting errors

--- test_calculator.py
+++ test_calculator.py
@@ -1,18 +1,17 @@
 """
-Tests for calc app
+Unit testing calc app
 """
+
 import calculator
 
 
 class TestCalculatorApp:
-	def test_add(self):
-		assert 5 == calculator.add(3, 2)
 
-	def test_subtract(self):
-		assert 5 == calculator.subtract(10,5)
+    def test_add(self):
+        assert 5 == calculator.add(1, 4)
 
-	def test_mult(self):
-		assert 12 == calculator.mult(3,4)
+    def test_subtract(self):
+        assert 2 == calculator.subtract(5, 3)
 
-	def test_div(self):
-		assert 2 == calculator.div(10,5)
+    def test_multiply(self):
+        assert 10 == calculator.multiply(2, 5)
